b12f97899295db8f1c6cb3d9bd227f3eb104c09b
Chloe <cehan16@gmail.com>
2016-11-28 00:24:13 -0500

    first commit

--- app/src/main/java/com/example/chloe/instantmessaging/Login.java
+++ app/src/main/java/com/example/chloe/instantmessaging/Login.java
@@ -1,8 +1,33 @@
 package com.example.chloe.instantmessaging;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 /**
  * Created by Chloe on 2016-11-27.
  */
 
-public class Login {
+public class Login extends Activity implements View.OnClickListener{
+    EditText inputID;
+    Button buttonJoin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        buttonJoin = (Button) findViewById(R.id.btnJoin);
+        buttonJoin.setOnClickListener(this);
+
+        //connect to the server
+        //direct to main page
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
--- app/src/main/java/com/example/chloe/instantmessaging/MainActivity.java
+++ app/src/main/java/com/example/chloe/instantmessaging/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.chloe.instantmessaging;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    Button buttonSend;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSend = (Button) findViewById(R.id.btnSend);
+        buttonSend.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
