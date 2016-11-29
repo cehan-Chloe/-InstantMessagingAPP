07e6a96e26e69269c73fc15bdc734e61c52f9d2e
Chloe <cehan16@gmail.com>
2016-11-29 09:18:17 -0500

    socket server

--- app/src/main/java/com/example/chloe/instantmessaging/Login.java
+++ app/src/main/java/com/example/chloe/instantmessaging/Login.java
@@ -1,33 +1,80 @@
 package com.example.chloe.instantmessaging;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by Chloe on 2016-11-27.
  */
 
-public class Login extends Activity implements View.OnClickListener{
+public class Login extends Activity {
     EditText inputID;
     Button buttonJoin;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        buttonJoin = (Button) findViewById(R.id.btnJoin);
-        buttonJoin.setOnClickListener(this);
+//        buttonJoin = (Button) findViewById(R.id.btnJoin);
+//        buttonJoin.setOnClickListener(this);
 
         //connect to the server
         //direct to main page
     }
 
-    @Override
-    public void onClick(View v) {
 
-    }
+//    @Override
+//    public void onClick(View v) {
+//
+//        if (v.getId() == R.id.btnJoin){
+//
+
+//            // get The User name and Password
+//
+//            userID = inputID.getText().toString();
+//            String password = inputPassword.getText().toString();
+//
+//
+//            // fetch the Password form database for respective user name
+//            Cursor cursor=db.getQTData(userID);
+//            String storedPassword = "";
+//            if (cursor != null && cursor.moveToFirst()) {
+//                do{
+//                    storedPassword = cursor.getString(cursor.getColumnIndex("password"));
+//                }while (cursor.moveToNext());
+//                cursor.close();
+//            }
+//
+//
+//            if (storedPassword.equals(password)){
+//                try {
+//                    Intent quiz = new Intent(QTLogin.this, QTHome.class);
+//                    startActivity(quiz);
+//                }catch(Exception e){
+//                    e.printStackTrace();
+//                }
+//            }
+//            else{
+//                Context context = getApplicationContext();
+//                CharSequence text = "The ID or password is wrong!";
+//                int duration = Toast.LENGTH_SHORT;
+//
+//                Toast toast = Toast.makeText(context, text, duration);
+//                toast.show();
+//            }
+//        }
+//    }
+//
+//    public String getID(){
+//        return String.valueOf(userID);
+//    }
 }
--- app/src/main/java/com/example/chloe/instantmessaging/MainActivity.java
+++ app/src/main/java/com/example/chloe/instantmessaging/MainActivity.java
@@ -1,27 +1,123 @@
 package com.example.chloe.instantmessaging;
 
-import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
-    Button buttonSend;
-
+public class MainActivity extends AppCompatActivity {
+    EditText ip;
+    EditText editText;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ip = (EditText) findViewById(R.id.ip);
+        editText = (EditText) findViewById(R.id.editMessage);
+        text = (TextView) findViewById(R.id.message);
 
-        buttonSend = (Button) findViewById(R.id.btnSend);
-        buttonSend.setOnClickListener(this);
+        findViewById(R.id.connect).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                connect();
+            }
+        });
+
+        findViewById(R.id.buttonSend).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send();
+            }
+        });
     }
 
     @Override
-    public void onClick(View v) {
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+    }
 
+    //---------------------------------
+
+    Socket socket = null;
+    BufferedReader br = null;
+    BufferedWriter bw = null;
+
+    public void connect() {
+        String ipAddress = ip.getText().toString();
+
+        AsyncTask<String, String, Void> read = new AsyncTask<String, String, Void>() {
+            @Override
+            protected Void doInBackground(String... params) {
+                try {
+                    socket = new Socket(params[0], 12345);
+                    br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                    publishProgress("@success");
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+//                    Toast.makeText(MainActivity.this, "cannot connect", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+//                    Toast.makeText(MainActivity.this, "cannot connect", Toast.LENGTH_SHORT).show();
+                }
+
+
+                try {
+                    String line;
+                    if(br != null) {
+                        while ((line = br.readLine()) != null) {
+                            publishProgress(line);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onProgressUpdate(String... values) {
+                if(values[0].equals("@success")) {
+                    Toast.makeText(MainActivity.this, "connected", Toast.LENGTH_SHORT).show();
+                }
+                text.append("she say: " + values[0] + "\n");
+                super.onProgressUpdate(values);
+            }
+
+        };
+        read.execute(ipAddress);
+    }
+
+
+    public void send() {
+        try {
+            if(bw != null) {
+                text.append("me: " + editText.getText().toString() + "\n");
+                bw.write(editText.getText().toString() + "\n");
+                bw.flush();
+                editText.setText("");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
